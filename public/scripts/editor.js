3980a0bb2537923aba5e0dd263dec58fbbf16946
Margot <margaretsobota@gmail.com>
2021-05-09 20:50:30 -0500

    lots done

--- public/scripts/editor.js
+++ public/scripts/editor.js
@@ -11,6 +11,60 @@ const initResetButton = () => {
     };
 };
 
+const getDoctors = async () => {
+  try {
+    const response = await fetch('http://localhost:8081/doctors',  {
+        method: 'GET'
+    });
+    if (response.ok) {
+        const data = await response.json();
+        data.forEach(doctor => {
+          document.querySelector("#doctors-section").innerHTML += `
+            <li>
+              <a id=${doctor._id} onclick="handleDoctorSelect(event)"> ${doctor.name} </a>
+            </li>
+          `;
+        });
+    } else {
+        console.log('Bad request');
+    };
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+const getDoctor = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:8081/doctors/${id}`,  {
+        method: 'GET'
+    });
+    if (response.ok) {
+        const data = await response.json();
+        document.querySelector("#doctor").innerHTML = `
+        <h1>
+          ${data.name}
+        </h1>
+        <h3>
+          Seasons: ${data.seasons}
+        </h3>
+        <img src=${data.image_url} />
+        `;
+    } else {
+        console.log('Bad request');
+    };
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+const handleDoctorSelect = (ev) => {
+  console.log("hi", ev.target);
+  const id = ev.target.id;
+  getDoctor(id);
+  console.log(id);
+}
+
+getDoctors();
 
 
 // invoke this function when the page loads:
