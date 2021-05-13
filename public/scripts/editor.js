e521564ffaa823b421ffbc1ce3ff14cd005c8f24
Margot <margaretsobota@gmail.com>
2021-05-12 19:37:44 -0500

    companions list

--- public/scripts/editor.js
+++ public/scripts/editor.js
@@ -21,7 +21,9 @@ const getDoctors = async () => {
         data.forEach(doctor => {
           document.querySelector("#doctors-section").innerHTML += `
             <li>
-              <a id=${doctor._id} onclick="handleDoctorSelect(event)"> ${doctor.name} </a>
+              <button class="doctor-button" id=${doctor._id} onclick="handleDoctorSelect(event)">
+                ${doctor.name}
+              </button>
             </li>
           `;
         });
@@ -57,10 +59,38 @@ const getDoctor = async (id) => {
   }
 }
 
+const getDoctorCompanions = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:8081/doctors/${id}/companions`,  {
+        method: 'GET'
+    });
+    if (response.ok) {
+        const data = await response.json();
+        document.querySelector("#companions-list").innerHTML = "";
+        data.forEach((companion) => {
+          document.querySelector("#companions-list").innerHTML += `
+          <li class="companion-el">
+            <img class="companion-img" src=${companion.image_url} />
+            <h3>
+              ${companion.name}
+            </h3>
+          </li>
+          `;
+        });
+
+    } else {
+        console.log('Bad request');
+    };
+  } catch(err) {
+    console.log(err);
+  }
+}
+
 const handleDoctorSelect = (ev) => {
   console.log("hi", ev.target);
   const id = ev.target.id;
   getDoctor(id);
+  getDoctorCompanions(id);
   console.log(id);
 }
 
